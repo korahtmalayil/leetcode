d46fec133e8bd55e92116396fb4c37533eb393a2
Korah T Malayil <korahtmalayil@gmail.com>
2025-05-19 18:44:50 +0530

    Delete neetcode150/two_sum.py

--- neetcode150/two_sum.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        num_dict[nums[0]] = 0
-        for idx in range(1, len(nums)):
-            curr_num = nums[idx]
-            if target - curr_num in num_dict:
-                return [num_dict[target - curr_num], idx]
-            num_dict[curr_num] = idx
-        return False
